ae46bc7d04603bf06cba57c28b5fa057f14f05f9
Chadzero <ramey.chad@gmail.com>
2015-05-11 16:20:09 -0500

    Completing Week 2 Work
    Finishing the problem set 3.

--- EDx6001/PS3.py
+++ EDx6001/PS3.py
@@ -1,1 +1,34 @@
 __author__ = 'Chad_Ramey'
+
+
+def f(x):
+    import math
+    return 10*math.e**(math.log(0.5)/5.27 * x)
+
+def radiationExposure(start, stop, step):
+    '''
+    Computes and returns the amount of radiation exposed
+    to between the start and stop times. Calls the
+    function f (defined for you in the grading script)
+    to obtain the value of the function at any point.
+
+    start: integer, the time at which exposure begins
+    stop: integer, the time at which exposure ends
+    step: float, the width of each rectangle. You can assume that
+      the step size will always partition the space evenly.
+
+    returns: float, the amount of radiation exposed to
+      between start and stop times.
+    '''
+    total_area = 0
+
+    for i in range(int((stop - start) / step)):
+        total_area += f(start + i * step) * step
+
+    return total_area
+
+print "Radiation Exposure Results:"
+print radiationExposure(0, 5, 1)
+print radiationExposure(5, 11, 1)
+print radiationExposure(0, 11, 1)
+print radiationExposure(40, 100, 1.5)
